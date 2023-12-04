86a3ef954086fbaeb827950568e4be72690bfe9c
Tevfik Baş (ETIYA) <tevfik.bas@etiya.com>
2023-12-04 15:49:45 +0300

    feat: ImageProvider added in FileData

--- lib/src/models/file_kind.dart
+++ lib/src/models/file_kind.dart
@@ -1,11 +1,15 @@
+import 'package:flutter/material.dart';
+
 class FileData {
   FileData({
     required this.url,
     required this.mime,
     required this.type,
     required this.subText,
+    this.imageUrl,
   });
   final String url;
+  final ImageProvider? imageUrl;
   final String mime;
   final String type;
   final String subText;
